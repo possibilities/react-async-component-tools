4cd71ee46ff3bc00248431ba883f315c4a013aca
Mike Bannister <notimpossiblemike@gmail.com>
2016-01-03 22:11:30 -0800

    Commenting and renaming and cleanup

--- src/index.js
+++ src/index.js
@@ -2,30 +2,39 @@ import * as fsPath from 'path'
 import * as t from 'babel-types'
 import template from 'babel-template'
 
-const programImports = (programNode) => {
+// Get all of the import declarations from a program
+const extractProgramImports = (programNode) => {
   return programNode.body.filter(bodyNode => t.isImportDeclaration(bodyNode))
 }
 
+// Build up and inject an import statement for loading the decorator
 const insertDecoratorImportIntoProgramBody = (programNode) => {
+  // Build it up
   const buildImport = template(`const { IMPORT_NAME } = require(SOURCE)`)
-
   const decoratorImport = buildImport({
     IMPORT_NAME: t.identifier('injectAsyncComponent'),
     SOURCE: t.stringLiteral('babel-plugin-async-component/lib/AsyncComponentContainer')
   })
 
+  // And inject it
   programNode.body = [ decoratorImport, ...programNode.body ]
 }
 
+// The ambient `asyncLoadingElement` decorator is used to provide a JSX element.
+// Here we extract that argument for use in the decorator we eventually inject.
 const extractLoadingElementFromDecorator = (decorator) => {
   const misuseErrorMessage = '`@asyncLoadingElement` decorator requires a '
     + 'single argument which must be a JSX tag'
 
+  // Error out when no loading element is specified
   if (decorator.expression.arguments.length !== 1) {
     throw Error(misuseErrorMessage)
   }
 
+  // Extract the loading element
   const [ loadingElement ] = decorator.expression.arguments
+
+  // Validate the loading element
   if (!loadingElement || !t.isJSXElement(loadingElement)) {
     throw Error(misuseErrorMessage)
   }
@@ -33,16 +42,23 @@ const extractLoadingElementFromDecorator = (decorator) => {
   return loadingElement
 }
 
+// The ambient `asyncComponent` decorators are used to provide the identifiers
+// of each component class we want to be loaded asynchronously. Here we extract
+// that argument from a single decorator for use in the decorator we eventually
+// inject.
 const extractComponentNameFromDecorator = (decorator) => {
   const misuseErrorMessage = '`@asyncComponent` decorator requires a single '
     + 'argument which must be an identifier'
 
+  // Error out if we don't have any arguments
   if (decorator.expression.arguments.length !== 1) {
     throw Error(misuseErrorMessage)
   }
 
+  // Extract the identifier
   const [ asyncComponentIdentifier ] = decorator.expression.arguments
 
+  // Validate the identifier
   if (!asyncComponentIdentifier || !t.isIdentifier(asyncComponentIdentifier)) {
     throw Error(misuseErrorMessage)
   }
@@ -50,10 +66,13 @@ const extractComponentNameFromDecorator = (decorator) => {
   return asyncComponentIdentifier.name
 }
 
+// Extract useful arguments from an array of `asyncComponent` decorators
 const extractComponentNamesFromDecorators = (decorators) => {
   return decorators.map(extractComponentNameFromDecorator)
 }
 
+// Get all of a program's import statements that are implicated by
+// `asyncComponent` decorators
 const filterAsyncImports = (programImports, asyncComponentNames) => {
   return programImports.filter((programImport) => {
     // TODO support more diverse specifiers like { Foo }, and { Foo, Bar }
@@ -62,6 +81,18 @@ const filterAsyncImports = (programImports, asyncComponentNames) => {
   })
 }
 
+// Generates a lookup table of import name to source
+// ```
+// import Foo from '../Foo'
+// import Bar from '../Bar'
+// ```
+// yields:
+// ```
+// {
+//   Foo: '../Foo',
+//   Bar: '../Bar',
+// }
+// ```
 const importNamesToSources = (asyncImports) => {
   return asyncImports.map(asyncImport => {
     // TODO support more diverse specifiers like { Foo }, and { Foo, Bar }
@@ -73,13 +104,16 @@ const importNamesToSources = (asyncImports) => {
   })
 }
 
+// Given a list of imports remove them from a given program
 const deleteImportsFromProgram = (deletableImports, programNode) => {
   return programNode.body = programNode.body.filter(bodyNode => {
     return deletableImports.indexOf(bodyNode) === -1
   })
 }
 
-const applyDecoratorToComponentClass = (decorators, loadingElement, decoratorInfo) => {
+// Given all the appropriate metadata inject a `injectAsyncComponent` decorator
+// around a given react component
+const applyDecoratorToComponentClass = (componentClass, loadingElement, decoratorInfo) => {
   const buildDecorator = template(`
     injectAsyncComponent(IMPORT_NAME_STRING, LOADING_ELEMENT, (onReady) => {
       require.ensure(SOURCE_PATH, (require) => {
@@ -94,37 +128,77 @@ const applyDecoratorToComponentClass = (decorators, loadingElement, decoratorInf
     SOURCE_PATH: t.stringLiteral(decoratorInfo.source),
     LOADING_ELEMENT: loadingElement
   })
-  decorators.push(decorator)
+  componentClass.decorators.push(decorator)
 }
 
-const asyncComponentDecoratorVisitor = {
-  ClassDeclaration(path) {
-    const { programNode } = this
-    const { node: classDeclarationNode } = path
-    const decorators = classDeclarationNode.decorators || []
+// This visitor modifies the render method so it properly destructures the
+// component prop from `this.props` so the method will transparently use the
+// component injected by the wrapping component.
+const methodVisitor = {
+  ClassMethod(classMethodPath) {
+    const { asyncComponentNames } = this
 
-    if (!decorators.length) {
+    // Bail if we're not in `render()`
+    if (classMethodPath.node.key.name !== 'render') {
       return
     }
 
-    // Collect the component decorators
-    const asyncComponentDecorators = decorators.filter((decorator) => {
+    // Build up the fragment
+    const extractInjectedComponent = asyncComponentNames.map((componentName) => {
+      const extractInjectedComponentBuilder = template(`
+        const { COMPONENT_NAME } = this.props
+      `)
+
+      const extractInjectedComponentExpression = extractInjectedComponentBuilder({
+        COMPONENT_NAME: t.identifier(componentName)
+      })
+
+      return extractInjectedComponentExpression
+    })
+
+    // Inject the generated fragment
+    classMethodPath.node.body.body = [
+      ...extractInjectedComponent,
+      ...classMethodPath.node.body.body,
+    ]
+  }
+}
+
+// This is where most of the business happens. In a nutshell we read many values
+// from the AST and use them to replace the "ambient decorators" with the
+// the appropriate webpack "code splitting" logic.
+// See: https://webpack.github.io/docs/code-splitting.html
+const classVisitor = {
+  ClassDeclaration(classDeclarationPath) {
+    // Destructure values we'll need
+    const { node: classDeclarationNode } = classDeclarationPath
+    const { decorators } = classDeclarationNode
+    const { programPath } = this
+    const { node: programNode } = programPath
+
+    // Bail if there are no decorators
+    if (!decorators) {
+      return
+    }
+
+    // Collect the `asynComponent` decorators
+    const componentDecorators = decorators.filter((decorator) => {
       return decorator.expression.callee.name === 'asyncComponent'
     })
 
-    // Get the loading element decorator
-    const asyncLoadingElementDecorator = decorators.find((decorator) => {
+    // Get the `asyncLoadingElement` decorator
+    const loadingElementDecorator = decorators.find((decorator) => {
       return decorator.expression.callee.name === 'asyncLoadingElement'
     })
 
-    // Skip out if we don't have any relavent decorators
-    if ( !asyncLoadingElementDecorator && !asyncComponentDecorators.length) {
+    // Bail if we don't have any relevant decorators
+    if ( !loadingElementDecorator && !componentDecorators.length) {
       return
     }
 
-    // Error out if we don't have one load element decorator and at least one
-    // component decorators
-    if (!asyncLoadingElementDecorator || !asyncComponentDecorators.length) {
+    // Error out if we don't have one `asyncLoadingElement` decorator and at
+    // least one `asyncComponent` decorator
+    if (!loadingElementDecorator || !componentDecorators.length) {
       throw new Error(''
         + 'One `@asyncLoadingElement` decorator is needed when a class is '
         + 'decorated with one or more `@asyncComponent` decorators and '
@@ -132,82 +206,67 @@ const asyncComponentDecoratorVisitor = {
       )
     }
 
-    insertDecoratorImportIntoProgramBody(programNode)
+    // Extract the info we need from the ambient decorators
+    const loadingElement = extractLoadingElementFromDecorator(
+      loadingElementDecorator
+    )
+    const asyncComponentNames = extractComponentNamesFromDecorators(
+      componentDecorators
+    )
 
-    const loadingElement = extractLoadingElementFromDecorator(asyncLoadingElementDecorator)
-    const asyncComponentNames = extractComponentNamesFromDecorators(asyncComponentDecorators)
+    // Figure out which of the program imports are associated with the
+    // ambient decorators
+    const programImports = extractProgramImports(programPath.node)
+    const asyncImports = filterAsyncImports(
+      programImports,
+      asyncComponentNames
+    )
 
-    const { allProgramImports } = this
-    const asyncImports = filterAsyncImports(allProgramImports, asyncComponentNames)
-
+    // Get a lookup table of import name to source
     const importNameToSourceLookup = importNamesToSources(asyncImports)
 
     // Delete the ambient loading element decorator
-    const loadingElementDecoratorIndex = decorators.indexOf(asyncLoadingElementDecorator)
+    const loadingElementDecoratorIndex = decorators.indexOf(loadingElementDecorator)
+    insertDecoratorImportIntoProgramBody(programNode)
     decorators.splice(loadingElementDecoratorIndex, 1)
 
     // Delete all of the ambient component decorators
-    asyncComponentDecorators.forEach((decorator) => {
+    componentDecorators.forEach((decorator) => {
       const decoratorIndex = decorators.indexOf(decorator)
       decorators.splice(decoratorIndex, 1)
     })
 
+    // Get rid of the real imports as these will be loaded on demand
     deleteImportsFromProgram(asyncImports, programNode)
 
-    const applyDecorator = applyDecoratorToComponentClass.bind(
-      null,
-      decorators,
+    // Add the real decorator to the component class
+    importNameToSourceLookup.forEach(applyDecoratorToComponentClass.bind(null,
+      classDeclarationNode,
       loadingElement
-    )
+    ))
 
-    importNameToSourceLookup.forEach(applyDecorator)
-
-    path.traverse(componentRenderMethodVisitor, {
+    // Drop into render method for additional modifications
+    // TODO make this more robust, i.e. component won't always be in `render()`
+    classDeclarationPath.traverse(methodVisitor, {
       asyncComponentNames
     })
   }
 }
 
-const componentRenderMethodVisitor = {
-  ClassMethod(path) {
-    if (path.node.key.name !== 'render') {
+// The main program visitor just makes sure we're in a module and shells out to
+// the `classVisitor`
+const programVisitor = {
+  Program(programPath) {
+    // Process ES6+ modules only
+    if (programPath.node.sourceType !== 'module') {
       return
     }
 
-    const { asyncComponentNames } = this
-
-    const extractValueExpressions = asyncComponentNames.map((componentName) => {
-      const extractValueBuilder = template(`
-        const { COMPONENT_NAME } = this.props
-      `)
-
-      const extractValueExpression = extractValueBuilder({
-        COMPONENT_NAME: t.identifier(componentName)
-      })
-
-      return extractValueExpression
-    })
-
-    path.node.body.body = [ ...extractValueExpressions, ...path.node.body.body ]
+    // Start by exploring the classes
+    programPath.traverse(classVisitor, { programPath })
   }
 }
 
 export default function() {
-  return {
-    visitor: {
-      Program(path) {
-        // Process ES6+ modules only
-        if (path.node.sourceType !== 'module') {
-          return
-        }
-
-        const allProgramImports = programImports(path.node)
-
-        path.traverse(asyncComponentDecoratorVisitor, {
-          programNode: path.node,
-          allProgramImports,
-        })
-      }
-    }
-  }
+  return { visitor: programVisitor }
 }
